e069ebb321082abb4d838d53caa187a3c15f3df0
Rahul Roy <lanoire141018@gmail.com>
2024-01-12 11:22:10 +0800

    Customer Controller and routes with JWT

--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -1,6 +1,7 @@
 const cart = require("../controller/cartsController");
 
 const router = require("express").Router();
+const validateToken = require('../middleware/AuthMiddleware');
 
 router.get("/carts", cart.findAllCarts);
 
