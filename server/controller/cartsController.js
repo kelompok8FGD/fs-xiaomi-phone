60e77fdf0c84b21555e7e80a389f3736a57cacd5
Maria Jose VB <vb.maria@outlook.com>
2024-01-12 20:06:14 +0700

    Update cart backend

--- server/controller/cartsController.js
+++ server/controller/cartsController.js
@@ -2,22 +2,72 @@ const db = require("../models");
 const Cart = db.CartModel;
 const { Op } = require("sequelize");
 
-//Menampilkan semua cart
-const findAllCarts = async (req, res) => {
-    try {
-      const dataCarts = await Cart.findAll();
-  
-      const result = {
-        status: "ok",
-        data: dataCarts,
-      };
-      res.json(result);
-    } catch (error) {
-      console.log(error, "<<<-- Error finding all carts");
-    }
-  };
+const addToCart = async (req, res) => {
+  try {
+      const { id_product, qty, price } = req.body;
+      const id_customer = req.user.id; //  customer ID available in req.user
+
+      // Check if the product is already in the cart
+      const existingCartItem = await CartModel.findOne({
+          where: {
+              id_product,
+              id_customer,
+          },
+      });
+
+      if (existingCartItem) {
+          // If the product is already in the cart, update the quantity
+          existingCartItem.qty += qty;
+          await existingCartItem.save();
+      } else {
+          // If the product is not in the cart, add a new entry
+          await CartModel.create({
+              id_product,
+              id_customer,
+              qty,
+              price,
+          });
+      }
+
+      res.json({
+          status: "ok",
+          message: "Item added to the cart successfully",
+      });
+  } catch (error) {
+      console.error("Error adding item to cart:", error);
+      res.status(500).json({
+          status: "failed",
+          message: "Internal Server Error",
+      });
+  }
+};
+
+
+const getCartItems = async (req, res) => {
+  try {
+      const id_customer = req.user.id; //  customer ID  in req.user
+
+      const cartItems = await Cart.findAll({
+          where: {
+              id_customer,
+          },
+      });
+
+      res.json({
+          status: "ok",
+          data: cartItems,
+      });
+  } catch (error) {
+      console.error("Error getting cart items:", error);
+      res.status(500).json({
+          status: "failed",
+          message: "Internal Server Error",
+      });
+  }
+};
 
 module.exports = {
-    findAllCarts,
+  addToCart,  
+  getCartItems
   };
   
