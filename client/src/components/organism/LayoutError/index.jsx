6412ba229aa42eb792adf2f4bff38421fb1d6fdc
Rifqi <rifqitaufiqurrohman1@gmail.com>
2024-01-20 12:12:55 +0700

    Update Pages error & xiaomi

--- client/src/components/organism/LayoutError/index.jsx
+++ client/src/components/organism/LayoutError/index.jsx
@@ -1,13 +1,17 @@
 import BannerError from "../../Atoms/BannerError";
-import Judul from "../../molecule/Error/Judul";
 import ListCard from "../../molecule/Error/Card/ListCard";
 
 const LayoutError = () => {
   return (
     <>
-      <BannerError ErrorPicDesktop="/error/page-404.png" />
+      <BannerError ErrorPicDesktop="https://firebasestorage.googleapis.com/v0/b/tutorial-1ad91.appspot.com/o/xiaomi%2Fpage-404.png?alt=media&token=a64d3e39-3765-49b5-aa69-5061731bf8b7" />
 
-      <Judul />
+      <section id="judul_card" className="text-center font-inter">
+        <h1 className="text-slate-500 text-xl">
+          Masih bingung? Coba lihat produk premium ini, mungkin ada yang Anda
+          suka!
+        </h1>
+      </section>
 
       <ListCard />
     </>
