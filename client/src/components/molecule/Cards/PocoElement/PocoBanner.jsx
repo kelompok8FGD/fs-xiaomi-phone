359b7de7312fcd56c87d173cf2e277650629b919
rakanugraha18 <rakanugraha2@gmail.com>
2024-01-24 17:56:40 +0700

    fix poco product

--- client/src/components/molecule/Cards/PocoElement/PocoBanner.jsx
+++ client/src/components/molecule/Cards/PocoElement/PocoBanner.jsx
@@ -39,58 +39,6 @@ export default function PocoBanner() {
     <>
       <div className="flex flex-col md:hover:cursor-pointer">
         {currentBannerImg.map((poco) => (
-          <div
-            key={poco.id_product}
-            className="flex flex-col items-center relative"
-          >
-            <div
-              key={poco.id_product}
-              className="absolute text-center w-[370px] pt-10 md:text-start md:left-[10%] md:top-[60px] md:w-[600px]"
-            >
-              <ProductTitleFlagship
-                Title={poco.name_product}
-                Specs={poco.specification}
-                CurrentPrice={poco.price}
-              ></ProductTitleFlagship>
-              <div className="flex flex-row gap-2">
-                <CustomButton
-                  to="/cart"
-                  text="Beli Sekarang"
-                  intent="light"
-                  rounded="yes"
-                  hover="bg_soft"
-                  onClick={() =>
-                    dispatch(
-                      addToCart({
-                        id: poco.id_product,
-                        name: poco.name_product,
-                        image: poco.image,
-                        price: poco.price,
-                      })
-                    )
-                  }
-                />
-                <LearnMoreButton
-                  id={poco.id_product}
-                  text="Learn More >"
-                  intent="white_nobg"
-                  rounded="yes"
-                  border="no"
-                  icon="text_first"
-                  hover="underline"
-                />
-              </div>
-            </div>
-            <ProductImgFlagship
-              id={poco.id_product}
-              PhonePicDesktop="https://firebasestorage.googleapis.com/v0/b/xiaomi-phone-e544c.appspot.com/o/poco_f5%2Fbanner_poco_F5_panjang_desktop.jpg?alt=media&token=54a466d1-7760-40c2-ac1d-253a983e3eb4"
-              PhonePicMobile="https://firebasestorage.googleapis.com/v0/b/xiaomi-phone-e544c.appspot.com/o/poco_f5%2Fbanner_poco_F5_panjang_mobile.jpg?alt=media&token=d09ec3e7-209b-44d9-8547-6c17198d4131"
-            ></ProductImgFlagship>
-          </div>
-        ))}
-      </div>
-      {/* <div className="flex flex-col md:hover:cursor-pointer">
-        {currentBannerImg.map((poco) => (
           <>
             <div key={poco.id} className="flex flex-col items-center relative">
               <div className="absolute text-center w-[370px] pt-10 md:text-start md:left-[10%] md:top-[60px] md:w-[600px]">
@@ -136,7 +84,7 @@ export default function PocoBanner() {
             ></ProductImgFlagship>
           </>
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
