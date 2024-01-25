d4eabf901175929e63959539eefbd556623ae463
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-01-25 17:57:18 +0800

    Fixing LongCard and BannerFlagship at Smartphone Page

--- client/src/components/Atoms/LongCard.jsx/index.jsx
+++ client/src/components/Atoms/LongCard.jsx/index.jsx
@@ -11,8 +11,8 @@ import ProductLongCardSmartphone from "../../Atoms/InsideCard/ProductLongCardSma
 const LongCard = (props) => {
     const {styling} = props;
     const [dataXiaomi, setDataXiaomi] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [postsPerPage, setPostPerPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(9);
+    const [postsPerPage, setPostPerPage] = useState(10);
 
     const dispatch = useDispatch();
 
