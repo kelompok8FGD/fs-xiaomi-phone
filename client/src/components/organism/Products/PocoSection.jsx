b4feb70d2bca33c290c3b0b48be09ea7fa1da668
Maria Jose VB <vb.maria@outlook.com>
2024-01-27 22:47:08 +0700

    Reusable Product Cards component

--- client/src/components/organism/Products/PocoSection.jsx
+++ client/src/components/organism/Products/PocoSection.jsx
@@ -1,17 +1,21 @@
 import PocoBanner from "../../molecule/Cards/PocoElement/PocoBanner";
 import PocoLargeCard from "../../molecule/Cards/PocoElement/PocoLargeCard";
 import PocoSmallCard from "../../molecule/Cards/PocoElement/PocoSmallCard";
+import ProductCards from "../../molecule/Cards/ProductCards";
+
 
 export default function PocoSection() {
   return (
     <>
       {/* Banner Flagship */}
       <PocoBanner />
-      <section className="flex flex-wrap bg-[#F7F7F7]">
+      <section className="w-full flex flex-col flex-wrap bg-[#F7F7F7]">
         {/* Large Card */}
-        <PocoLargeCard />
+        <ProductCards category="poco" mdCols={2} startfromproduct={0} endatproduct={2} />  
         {/* Small Card Start */}
-        <PocoSmallCard />
+        <ProductCards category="poco" mdCols={2} lgCols={3} xlCols={3} startfromproduct={3} endatproduct={7} />
+
+      
       </section>
     </>
   );
